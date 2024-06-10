97ac2784621a3e440d8aaf563eadde8e40ecc743
persadaditya <phedeveng@gmail.com>
2024-06-10 11:26:02 +0700

    create counter view

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -4,13 +4,15 @@ import 'package:learn_stacked/ui/views/home/home_view.dart';
 import 'package:learn_stacked/ui/views/startup/startup_view.dart';
 import 'package:stacked/stacked_annotations.dart';
 import 'package:stacked_services/stacked_services.dart';
+import 'package:learn_stacked/ui/views/counter/counter_view.dart';
 // @stacked-import
 
 @StackedApp(
   routes: [
     MaterialRoute(page: HomeView),
     MaterialRoute(page: StartupView),
-    // @stacked-route
+    MaterialRoute(page: CounterView),
+// @stacked-route
   ],
   dependencies: [
     LazySingleton(classType: BottomSheetService),
---
+++ lib/ui/views/counter/counter_view.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+
+import 'counter_viewmodel.dart';
+
+class CounterView extends StackedView<CounterViewModel> {
+  const CounterView({Key? key}) : super(key: key);
+
+  @override
+  Widget builder(
+    BuildContext context,
+    CounterViewModel viewModel,
+    Widget? child,
+  ) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Container(
+        padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+        child: Center(
+          child: Text('${viewModel.counter}',
+            style: const TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: viewModel.incrementCounter,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+
+  @override
+  CounterViewModel viewModelBuilder(
+    BuildContext context,
+  ) =>
+      CounterViewModel();
+}
